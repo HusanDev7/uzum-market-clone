06df0a4092f86763fe90deae01c37ed66a4d81f7
Husan <shomurodovhusan130@gmail.com>
2024-06-15 17:20:28 +0500

    add language but i have erorr))

--- src/main.js
+++ src/main.js
@@ -1,30 +1,29 @@
-// main.scss code
-import "./assets/styles/main.scss"
-// main.scss code
+import "./assets/styles/main.scss";
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import {createI18n} from "vue-i18n"
+import { createI18n } from "vue-i18n";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-import uz from "./locales/uz.json"
-import ru from "./locales/ru.json"
+import UZ from "./locales/uz.json";
+import RU from "./locales/ru.json";
 
-const i18n = createI18n({
-    locale: 'uz',
-    fallbackLocal: 'uz',
-    massages: {
-        uz,
-        ru
+export const i18n = createI18n({
+    legacy: false, // Отключите legacy mode
+    locale: localStorage.getItem('lang') || 'UZ', // Исправлено localStorage.getItem('lang')
+    messages: {
+        UZ: UZ,
+        RU: RU
     }
-})
+});
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
-const pinia = createPinia()
-app.use(pinia)
-app.use(router)
-app.use(i18n)
+const app = createApp(App);
+const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
-app.mount('#app')
+app.use(pinia);
+app.use(router);
+app.use(i18n);
+
+app.mount('#app');
